87d552b77ee3106d2462b75916f853f534830a08
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-02-26 17:26:56 -0500

    commit tree b4b736d61f12dae73c292b64e5ed918420a5e407

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 11;
+let num4 = 7;
 
-// Write your code here
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random());
+let mod = num3 % num4;
+let max = Math.max(20, 1, 11, 5);
